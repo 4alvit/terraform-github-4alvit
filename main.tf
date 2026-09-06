e222dc475b9bffb7f5c233ebefb7364d97a579ee
4alvit <alvit.work+4alvit@gmail.com>
2026-09-05 20:24:46 -0700

    revert: restore TFC organization to victron-venus
    HashiCorp account rename is not the HCP Terraform / TFC org.
    Keep cloud.organization = "victron-venus" and workspace
    github-4alvit-infrastructure.

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.15.7"
 
   cloud {
-    organization = "alvit"
+    organization = "victron-venus"
 
     workspaces {
       name = "github-4alvit-infrastructure"
